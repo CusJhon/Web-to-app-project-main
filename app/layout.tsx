79e36b805b75caea3699c08da6535bdc6120dcf7
CusJhon <cusjhon52@gmail.com>
2026-04-27 18:48:15 +0700

    Initial commit via RepoFlow Smart Upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: 'Web2Native - SANN404 FORUM',
-  description: 'Convert your website into a native Android and iOS application easily. Developed by SANN404 FORUM.',
+  title: 'Web2Native - JHON FORUM',
+  description: 'Convert your website into a native Android and iOS application easily. Developed by JHON FORUM.',
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
